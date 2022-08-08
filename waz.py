88dbe625e893116b6153d9bfcd1cfc78607d8193
jhoppe99 <divonworld@gmail.com>
2022-08-08 19:56:48 +0200

    first commit

---
+++ waz.py
@@ -0,0 +1,51 @@
+board_height = 5
+board_width = 6
+snake_width = 3
+empty_space = '.'
+snake = '*'
+speed_sec = 0.5
+
+sideways = empty_space*board_width
+
+number = 0
+while number != board_height:
+    print(sideways)
+    number = number + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+# jamaweza = {'1': ' ', '2': ' ', '3':' ',
+#           '6': ' ', '5': ' ', '4':' ',
+#           '7': ' ', '8': ' ', '9':' '}
+
+#def printplansza(plansza):
+#    print(plansza['1'] + 'I' + plansza['2'] + 'I' + plansza['3'])
+#    print('-+-+-')
+#    print(plansza['4'] + 'I' + plansza['5'] + 'I' + plansza['6'])
+#    print('-+-+-')
+#    print(plansza['7'] + 'I' + plansza['8'] + 'I' + plansza['9'])
+
+
+# printplansza(jamaweza)
+
+# waz1 = input("1 segment weza: ")
+# waz2 = input("2 segment weza: ")
+# waz3 = input("3 segment weza: ")
+
+# jamaweza[waz1] = '*'
+# jamaweza[waz2] = '*'
+# jamaweza[waz3] = '*'
+
+# printplansza(jamaweza)
+
+
