52f982c947fb33fecd026efe4194877791d7929e
jhoppe99 <divonworld@gmail.com>
2022-08-09 14:43:44 +0200

    new_board
    huge changes in board printing, plus changes in code structure.

--- waz.py
+++ waz.py
@@ -1,51 +1,28 @@
+#!/usr/bin/python3.8
+
 board_height = 5
-board_width = 6
+board_width = 19
 snake_width = 3
 empty_space = '.'
 snake = '*'
 speed_sec = 0.5
 
-sideways = empty_space*board_width
+def get_empty_board(width: int, height: int):
 
-number = 0
-while number != board_height:
-    print(sideways)
-    number = number + 1
+    pass
 
 
+# board = [[empty_space]*board_width]*board_height
+
+# for row in board:
+#     print(row)
 
 
+    
+if __name__ == '__main__':
+    board = get_empty_board(width=board_width, height=board_height)
+    print(board)
 
 
-
-
-
-
-
-
-
-# jamaweza = {'1': ' ', '2': ' ', '3':' ',
-#           '6': ' ', '5': ' ', '4':' ',
-#           '7': ' ', '8': ' ', '9':' '}
-
-#def printplansza(plansza):
-#    print(plansza['1'] + 'I' + plansza['2'] + 'I' + plansza['3'])
-#    print('-+-+-')
-#    print(plansza['4'] + 'I' + plansza['5'] + 'I' + plansza['6'])
-#    print('-+-+-')
-#    print(plansza['7'] + 'I' + plansza['8'] + 'I' + plansza['9'])
-
-
-# printplansza(jamaweza)
-
-# waz1 = input("1 segment weza: ")
-# waz2 = input("2 segment weza: ")
-# waz3 = input("3 segment weza: ")
-
-# jamaweza[waz1] = '*'
-# jamaweza[waz2] = '*'
-# jamaweza[waz3] = '*'
-
-# printplansza(jamaweza)
 
 
